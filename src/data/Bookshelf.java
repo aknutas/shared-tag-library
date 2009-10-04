58ef07b46c79986443c278e81afab8dc8cb85daf
shadowarts <shadowarts@e62d969a-9d79-11de-8ef5-057bcddc142d>
2009-10-04 20:56:58 +0000

    Finished up commenting... unions/etc coming in a few hours

--- src/data/Bookshelf.java
+++ src/data/Bookshelf.java
@@ -2,18 +2,112 @@ package data;
 
 import java.util.Iterator;
 
+/**
+ * The Bookshelf interface stores a set of books. The Bookshelf is main
+ * object used for searching through books.
+ * 
+ * @author Andrew Alm
+ */
 public interface Bookshelf {
+	
+	/**
+	 * Adds a book to the Bookshelf, if the Bookshelf already contains the book
+	 * then this method does nothing.
+	 * 
+	 * @param book the Book to add to the Bookshelf.
+	 * 
+	 * @throws IllegalArgumentException if the book given is null.
+	 */
 	public void insert(Book book) throws IllegalArgumentException;
-	public void remove(Book book) throws IllegalArgumentException;
+	
+	/**
+	 * Removes a book from the Bookshelf, if the Bookshelf does not contain the
+	 * book then this mtehod does nothing.
+	 * 
+	 * @param book the Book to remove from the Bookshelf.
+	 * 
+	 * @throws IllegalArgumentException if the book given is null.
+	 */
+	public boolean remove(Book book) throws IllegalArgumentException;
+	
+	/**
+	 * Determines whether the Bookshelf is empty. An empty bookshelf contains no
+	 * books and has a size of 0.
+	 * 
+	 * @return true if the Bookshelf is empty, otherwise false.
+	 */
 	public boolean empty();
+	
+	/**
+	 * Determines the size of the Bookshelf. A bookshelf's size is equal to the
+	 * number of book it conatins.
+	 * 
+	 * @return the number of books contained in the Bookshelf.
+	 */
 	public int size();
+	
+	/**
+	 * Retreives an iterator containing all books on the Bookshelf.
+	 * 
+	 * @return an iterator.
+	 */
 	public Iterator<Book> enumerate();
 
+	/**
+	 * Joins two Bookshelfs together into a new bookshelf. This method does not
+	 * change any old bookshelves.
+	 * 
+	 * @param shelf the shelf to union with.
+	 * 
+	 * @return a new Bookshelf object.
+	 * 
+	 * @throws IllegalArgumentException if the shelf given is null.
+	 */
 	public Bookshelf union(Bookshelf shelf) throws IllegalArgumentException;
+	
+	/**
+	 * Returns a bookshelf containing only that are contained in each Bookshelf
+	 * object.
+	 * 
+	 * @param shelf the shelf to intersect with.
+	 * 
+	 * @return a new Bookshelf obejct.
+	 * 
+	 * @throws IllegalArgumentException if the shelf given is null.
+	 */
 	public Bookshelf intersect(Bookshelf shelf) throws IllegalArgumentException;
+	
+	/**
+	 * Returns a bookshelf containly found only on this Bookshelf.
+	 * 
+	 * @param shelf the shelf to take the difference with.
+	 * 
+	 * @return a new Bookshelf object.
+	 * 
+	 * @throws IllegalArgumentException if the shelf given is null.
+	 */
 	public Bookshelf difference(Bookshelf shelf) throws IllegalArgumentException;
-	public Bookshelf subset(Bookshelf shelf, Book book) throws IllegalArgumentException;
+	
+	/**
+	 * Returns a bookshelf containing a subset of books which match the given book.
+	 * 
+	 * @param book the book to model the new bookshelf after.
+	 * 
+	 * @return a new Bookshelf object.
+	 * 
+	 * @throws IllegalArgumentException if the book given is null.
+	 */
+	public Bookshelf subset(Book book) throws IllegalArgumentException;
 
+	/**
+	 * to be removed
+	 * @return none
+	 */
 	public String getName();
+	
+	/**
+	 * to be removed
+	 * @return none
+	 */
 	public String setName(String name) throws IllegalArgumentException;
 }
--- src/data/VirtualBookshelf.java
+++ src/data/VirtualBookshelf.java
@@ -1,24 +1,38 @@
 package data;
 
 import java.util.*;
+//import javax.jdo.annotations.PersistenceCapable;
 
-import javax.jdo.annotations.PersistenceCapable;
-
-@PersistenceCapable
+/**
+ * The VirtualBookshelf class implements the Bookshelf interface and is used
+ * for in-memory, temporary Bookshelf objects. Most temporary Bookshelves will
+ * be used for browsing/searching persistent bookshelves.
+ * 
+ * @author Andrew Alm
+ */
+//@PersistenceCapable
 public class VirtualBookshelf implements Bookshelf {
 
 	private String name;
 	private Set<Book> bookshelf;
 	private Set<Bookshelf> shelves;
 
-	public VirtualBookshelf(String name) throws IllegalArgumentException {
-		if(null == name)
-			throw new IllegalArgumentException("name cannot be null");
-		
+	/**
+	 * Creates a new Bookshelf object which contains no books.
+	 */
+	public VirtualBookshelf() {
 		this.bookshelf = new HashSet<Book>();
 		this.shelves = new HashSet<Bookshelf>();
 	}
 
+	/**
+	 * Inserts a book onto the VirtualBookshelf, if the book already exists
+	 * on the Bookshelf then this method does nothing.
+	 * 
+	 * @param book the book to add to the bookshelf.
+	 * 
+	 * @throws IllegalArgumentException if the book given is null.
+	 */
 	public void insert(Book book) throws IllegalArgumentException {
 		if(null == book)
 			throw new IllegalArgumentException("book cannot be null");
@@ -26,13 +40,30 @@ public class VirtualBookshelf implements Bookshelf {
 		this.bookshelf.add(book);
 	}
 
-	public void remove(Book book) throws IllegalArgumentException {
+	/**
+	 * Removes a book from the VirtualBookshelf, if and only if the book was
+	 * added to this VirtualBookshelf (and not another bookshelf contained
+	 * within). If the Bookshelf does not contain the given book, then this
+	 * method does nothing.
+	 * 
+	 * @param book the book to remove from the bookshelf.
+	 * 
+	 * @return true if the book was removed, otherwise false.
+	 * 
+	 * @throws IllegalArgumentException if the book given is null.
+	 */
+	public boolean remove(Book book) throws IllegalArgumentException {
 		if(null == book)
 			throw new IllegalArgumentException("book cannot be null");
 
-		this.bookshelf.add(book);
+		return false;
 	}
 
+	/**
+	 * Determines if the virtual bookshelf is empty.
+	 * 
+	 * @return true if the book shelf is empty, otherwise false.
+	 */
 	public boolean empty() {
 		for(Bookshelf shelf : this.shelves) {
 			if(!shelf.empty())
@@ -42,6 +73,13 @@ public class VirtualBookshelf implements Bookshelf {
 		return this.bookshelf.isEmpty();
 	}
 
+	/**
+	 * Determines the size of the bookshelf. A virtual bookshelf is the size of
+	 * all the books it contains plus the size of all bookshelf objects it
+	 * contains, the size may not reflect the exact number of books aviable.
+	 * 
+	 * @return the size of the shelf.
+	 */
 	public int size() {
 		int size = 0;
 
@@ -51,21 +89,25 @@ public class VirtualBookshelf implements Bookshelf {
 		return (size + this.bookshelf.size());
 	}
 
+	/**
+	 * Returns an Iterator of all the books on the bookshelf. This includes
+	 * all eligible books on bookshelves contained within this virtual 
+	 * bookshelf.
+	 * 
+	 * @return an Iterator of books
+	 */
 	public Iterator<Book> enumerate() {
 		// need an iterator that traverses other iterators
 		return null;
 	}
 
-	public String getName() {
-		return this.name;
-	}
-
-	public String setName(String name) throws IllegalArgumentException {
-		String temp = this.name;
-		this.name = name;
-		return name;
-	}
-
+	/**
+	 * Joins two Bookshelves in holy matromony 'til delete do it's part.
+	 * 
+	 * @param shelf the bookshelf to wed.
+	 * 
+	 * @throws IllegalArgumentException if the shelf given is null.
+	 */
 	public Bookshelf union(Bookshelf shelf) throws IllegalArgumentException {
 		if(null == shelf)
 			throw new IllegalArgumentException("shelf cannot be null");
@@ -73,11 +115,19 @@ public class VirtualBookshelf implements Bookshelf {
 		if(shelf instanceof VirtualBookshelf) {
 			// merge
 		}
-
-		// use virtual bookshelf to combine non-similar types
+		
+		
 		return null;
 	}
 
+	/**
+	 * Computes the intersection of two bookshelf objects and returns a new
+	 * Bookshelf.
+	 * 
+	 * @param shelf the bookshelf to intersect with.
+	 * 
+	 * @throws IllegalArgumentException if the shelf given is null.
+	 */
 	public Bookshelf intersect(Bookshelf shelf) {
 		if(null == shelf)
 			throw new IllegalArgumentException("shelf cannot be null");
@@ -89,6 +139,14 @@ public class VirtualBookshelf implements Bookshelf {
 		return null;
 	}
 
+	/**
+	 * Computes the difference of two bookshelfs returning a new bookshelf
+	 * containing the result.
+	 * 
+	 * @param shelf the bookshelf to difference with.
+	 * 
+	 * @throws IllegalArgumentException if the shelf given is null.
+	 */
 	public Bookshelf difference(Bookshelf shelf) {
 		if(null == shelf)
 			throw new IllegalArgumentException("shelf cannot be null");
@@ -96,24 +154,69 @@ public class VirtualBookshelf implements Bookshelf {
 		return null;
 	}
 
-	public Bookshelf subset(Bookshelf shelf, Book book) throws IllegalArgumentException {
+	/**
+	 * Computes the subset of books contained in a Bookshelf that match part of
+	 * the given book.
+	 * 
+	 * @param book the model book.
+	 * 
+	 * @throws IllegalArgumentException if the book given is null.
+	 */
+	public Bookshelf subset(Book book) throws IllegalArgumentException {
 		return null;
-
 	}
 
-	public void addBookshelf(Bookshelf shelf) throws IllegalArgumentException {
+	
+	/**
+	 * Adds a bookshelf to this virtual bookshelf, if the shelf already exists
+	 * in this VirtualBookshelf, then this method does nothing.
+	 *
+	 * @param shelf the shelf to add.
+	 * 
+	 * @throws IllegalArgumentException if the shelf given is null.
+	 */
+	protected void addBookshelf(Bookshelf shelf) throws IllegalArgumentException {
 		if(null == shelf)
 			throw new IllegalArgumentException("shelf cannot be null");
 
 		this.shelves.add(shelf);
 	}
 
-	public void removeBookshelf(Bookshelf shelf) throws IllegalArgumentException {
+	/**
+	 * Removes a bookshelf from this virtual bookshelf, if the VirtualBookshelf
+	 * already contains the shelf then this method does nothing.
+	 * 
+	 * @param shelf the shelf to remove from the VirtualBookshelf
+	 * 
+	 * @throws IllegalArgumentException if the shelf given is null.
+	 */
+	protected void removeBookshelf(Bookshelf shelf) throws IllegalArgumentException {
 		if(null == shelf)
 			throw new IllegalArgumentException("shelf cannot be null");
 		
 		this.shelves.remove(shelf);
 	}
+	
+	/**
+	 * Creates a shallow-copy clone of this bookshelf.
+	 * 
+	 * @return a new Bookshelf identical to this.
+	 */
+	protected VirtualBookshelf shallowCopy() {
+		return null;
+	}
+	
+	
+	public String getName() {
+		return this.name;
+	}
+
+	public String setName(String name) throws IllegalArgumentException {
+		String temp = this.name;
+		this.name = name;
+		return name;
+	}
+
 
 }
 
