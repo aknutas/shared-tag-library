14b80ec0edf89d2b50ad3352f1d3ab18ff5447d1
shingis <shingis@e62d969a-9d79-11de-8ef5-057bcddc142d>
2009-10-08 23:41:08 +0000

    Things work again, but I have to still rewrite the queries. Querying nested collections is tougher than I thought. Storing and fetching all shelves work again, though! (and that was the sprint target)

--- trunk/src/database/QueryBuilder.java
+++ trunk/src/database/QueryBuilder.java
@@ -27,23 +27,33 @@ public interface QueryBuilder {
 
   /**
    * Returns all bookshelves with a book object with this specific title.
+   * DOES NOT WORK YET
    * @return       List A list of shelves.
    * @param        booktitle Book title.
    */
-  public List<Bookshelf> shelfSearch( String booktitle );
+  public List<Bookshelf> shelfSearchByBookName( String booktitle );
+  
+  /**
+   * Returns all bookshelves with this specific property.
+   * DOES NOT WORK YET
+   * @return       List A list of shelves.
+   * @param        property Property name.
+   * @param        value Property value.
+   */
+  public List<Bookshelf> shelfSearchByProperty( String property, String value );
   
   /**
    * Returns all bookshelves.
-   * @return       List An array list of shelves.
+   * @return       List A list of shelves.
    */
   public List<Bookshelf> shelfList(   );
 
   /**
-   * Creates a new shelf. Takes the persisted shelf object as a parameter.
+   * Stores new shelf, or updates the database with the changes. Takes the persisted shelf object as a parameter.
    * @return       int Success status.
-   * @param        shelf
+   * @param        shelf The shelf to be stored or updated.
    */
-  public int shelfCreate( Bookshelf shelf );
+  public int shelfStore( Bookshelf shelf );
 
 
   /**
