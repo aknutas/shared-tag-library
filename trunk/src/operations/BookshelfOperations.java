2526e1e9304e5d5ff353586abd445d453f8a7dc9
ashmore.fire.bringer <ashmore.fire.bringer@e62d969a-9d79-11de-8ef5-057bcddc142d>
2009-10-09 06:12:32 +0000

    Finished unit testing of BookshelfOperations.union.

--- trunk/src/operations/BookshelfOperations.java
+++ trunk/src/operations/BookshelfOperations.java
@@ -19,7 +19,7 @@ public abstract class BookshelfOperations{
 		
 		if (null == shelfs) throw new IllegalArgumentException("collection cannot be null");
 		
-		VirtualBookshelf newBS = new VirtualBookshelf("");
+		VirtualBookshelf newBS = new VirtualBookshelf();
 		
 		for( Bookshelf s: shelfs ){
 			if (null == s) throw new IllegalArgumentException("Collection contains a null shelf");
@@ -32,87 +32,88 @@ public abstract class BookshelfOperations{
 	public static Bookshelf subset(Collection<Bookshelf> shelfs, Bookshelf basis) throws IllegalArgumentException{
 		
 		if (null == shelfs) throw new IllegalArgumentException("collection cannot be null");
-		
-		VirtualBookshelf newBS = new VirtualBookshelf("");
-		VirtualBookshelf temp;
+
+		VirtualBookshelf newBS = new VirtualBookshelf();
 		
 		Iterator<Bookshelf> shelfsIt = shelfs.iterator();
-		VirtualBookshelfIterator basisBooks; 
-		
-		if (basis instanceof VirtualBookshelf){
-			
+		Iterator<Book> basisBooks; 
+
+		if (isVirtual(basis)){
+
 			while(shelfsIt.hasNext()){
-			
-				Bookshelf tempBS = shelfsIt.next();
-				Iterator<Book> tempIt = tempBS.enumerate();
-				
-				while(tempIt.hasNext()){
-					
-					temp = (VirtualBookshelf) basis;
-					basisBooks = (VirtualBookshelfIterator)temp.enumerate();
-				
-					Book current = null;
-					Iterator<Map.Entry<String,Integer>> currentWeights;
-				
+
+				Bookshelf currentBS = shelfsIt.next();
+				Iterator<Book> currentBSIt = currentBS.enumerate();
+
+				while(currentBSIt.hasNext()){
+
+					basisBooks = basis.enumerate();
+
+					Book currentBasisBook;
+					Book currentBook = currentBSIt.next();
+
+					Iterator<Map.Entry<String,Integer>> currentBasisWeights;
+
 					while(basisBooks.hasNext()){
-					
-						current = basisBooks.next();
-						currentWeights = current.enumerateTags();
-					
-						while(currentWeights.hasNext()){
-							
-											
-						
+
+						currentBasisBook = basisBooks.next();
+						currentBasisWeights = currentBasisBook.enumerateTags();
+
+						while(currentBasisWeights.hasNext()){
+							Map.Entry<String,Integer> tag = currentBasisWeights.next();
+
+							if (currentBook.weight(tag.getKey()) < 0){	} //do nothing.
+							else if (currentBook.weight(tag.getKey()) > tag.getValue()/2)
+								newBS.insert(currentBook);
+
 						}
-					
+
 					}	
-					
+
 				}
-				
-				
+
 			}
-			
-			
+				
 		}
-		
+
 		return null;
 	}
-	
-	
-	public static void main(String []args){
-		
-		try{
-			BookshelfOperations.union(null);
-		}
-		catch (Exception e){
-			System.out.println(e.getMessage());
-		}
-		
-		VirtualBookshelf a = new VirtualBookshelf("");
-		VirtualBookshelf b = new VirtualBookshelf("");
-		VirtualBookshelf c = new VirtualBookshelf("");
-		VirtualBookshelf d = new VirtualBookshelf("");
-		
+
+	private static boolean isVirtual(Bookshelf shelf){
+
+		return shelf instanceof VirtualBookshelf;
+	}
+
+/*	public static void main(String []args){
+
+		VirtualBookshelf a = new VirtualBookshelf();
+		VirtualBookshelf b = new VirtualBookshelf();
+		VirtualBookshelf c = new VirtualBookshelf();
+		VirtualBookshelf d = new VirtualBookshelf();
+
 		a.insert(new VirtualBook("a", "b"));
 		a.insert(new VirtualBook("c", "d"));
 		a.insert(new VirtualBook("e", "f"));
 		a.insert(new VirtualBook("g", "h"));
 		a.insert(new VirtualBook("i", "j"));
 		a.insert(new VirtualBook("k", "l"));
-		
+		a.setName("a - l");
+
 		b.insert(new VirtualBook("m", "n"));
 		b.insert(new VirtualBook("o", "p"));
 		b.insert(new VirtualBook("q", "r"));
 		b.insert(new VirtualBook("s", "t"));
 		b.insert(new VirtualBook("u", "v"));
 		b.insert(new VirtualBook("w", "x"));
-		
+		b.setName("m - x");
+
 		c.insert(new VirtualBook("1", "2"));
 		c.insert(new VirtualBook("3", "4"));
 		c.insert(new VirtualBook("5", "6"));
 		c.insert(new VirtualBook("7", "8"));
 		c.insert(new VirtualBook("9", "10"));
 		c.insert(new VirtualBook("11", "12"));
+		c.setName("1 - 12");
 
 		d.insert(new VirtualBook("alpha", "beta"));
 		d.insert(new VirtualBook("alpha", "beta"));
@@ -120,46 +121,98 @@ public abstract class BookshelfOperations{
 		d.insert(new VirtualBook("alpha", "beta"));
 		d.insert(new VirtualBook("alpha", "beta"));
 		d.insert(new VirtualBook("alpha", "beta"));
-		
-		try{
-			Stack<Bookshelf> tmp = new Stack<Bookshelf>();
-			tmp.add(a);
-			tmp.add(null);
-			BookshelfOperations.union(tmp);
-		}
-		catch (Exception e){
-			System.out.println(e.getMessage());
-		}
-		
-		Stack<Bookshelf> stackOfBooks = new Stack<Bookshelf>();
-		stackOfBooks.add(a);
-		stackOfBooks.add(b);
-		
-		Bookshelf aUnionB = BookshelfOperations.union(stackOfBooks);
-		
-		Iterator<Book> itA = a.enumerate();
-		Iterator<Book> itB = b.enumerate();
-		
-		boolean two = true;
-		
-		while (itA.hasNext()){
-			Book temp = itA.next();
-			if (two && aUnionB.remove(temp)){}	//do nothing
+		d.setName("alphas and betas");
+
+		Stack<Bookshelf> tmp = new Stack<Bookshelf>();
+		tmp.add(a);
+		BookshelfOperations.union(tmp);
+
+			List<Bookshelf> stackOfBooks = new Vector<Bookshelf>();
+			stackOfBooks.add(a);
+			stackOfBooks.add(b);
+
+			Bookshelf aUnionB = BookshelfOperations.union(stackOfBooks);
+
+			Iterator<Book> itA = a.enumerate();
+			Iterator<Book> itB = b.enumerate();
+
+			boolean two = true;
+
+			while (two && itA.hasNext()){
+				Book temp = itA.next();
+				if (aUnionB.contains(temp)){}	//do nothing
+				else{
+					System.out.println("Failed on A: " + temp.getTitle() + "  " + temp.getAuthor());
+					two = false;
+				}
+
+			}
+			while (two & itB.hasNext()){
+				Book temp = itB.next();
+				if (aUnionB.contains(temp)){}	//do nothing
+				else{
+					System.out.println("Failed on B: " + temp.getTitle() + "  " + temp.getAuthor());
+					two = false;
+				}
+			}
+
+			if (!two)
+				System.out.println("Size 2: failed");
 			else
-				two = false;
-					
-		}
-		while (itB.hasNext()){
-			Book temp = itB.next();
-			if (two && aUnionB.remove(temp)){}	//do nothing
+				System.out.println("Size 2: passed");
+			
+			Stack<Bookshelf> stackOfBooks2 = new Stack<Bookshelf>();
+			stackOfBooks2.add(a);
+			stackOfBooks2.add(b);
+			stackOfBooks2.add(c);
+
+			Bookshelf abc = BookshelfOperations.union(stackOfBooks2);
+			
+			itA = a.enumerate();
+			itB = b.enumerate();
+			Iterator<Book> itC = c.enumerate();
+			
+
+			boolean three = true;
+
+			while (three && ( itA.hasNext() | itB.hasNext() | itC.hasNext() )){
+				
+				Book tempA;
+				Book tempB;
+				Book tempC;
+				
+				if (itA.hasNext())
+				{
+					tempA = itA.next();
+					if (abc.contains(tempA)){}	//do nothing
+					else{
+						System.out.println("Failed on: " + tempA.getTitle() + "  " + tempA.getAuthor());
+						three = false;
+					}
+				}
+				if (itB.hasNext())
+				{
+					tempB = itB.next();
+					if (abc.contains(tempB)){}	//do nothing
+					else{
+						System.out.println("Failed on: " + tempB.getTitle() + "  " + tempB.getAuthor());
+						three = false;
+					}
+				}
+				if (itC.hasNext())
+				{
+					tempC = itC.next();
+					if (abc.contains(tempC)){}	//do nothing
+					else{
+						System.out.println("Failed on: " + tempC.getTitle() + "  " + tempC.getAuthor());
+						three = false;
+					}
+				}
+			}
+
+			if (!three)
+				System.out.println("Size 3: failed");
 			else
-				two = false;
-		}
-		
-		if (!two)
-			System.out.println("Size: 2 failed");
-		
-		
-		
+				System.out.println("Size 3: passed");
+		}*/
 	}
-}
