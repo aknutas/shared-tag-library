5efc9774db7590ccdd048c394617f898ae83bc33
shingis <shingis@e62d969a-9d79-11de-8ef5-057bcddc142d>
2009-10-10 01:41:08 +0000

    Changed code to match project coding standards.

--- src/database/Access.java
+++ src/database/Access.java
@@ -3,65 +3,56 @@ package database;
 import java.util.ArrayList;
 import java.util.List;
 
-
 /**
- * Interface Access
- * The singleton object that both manages and queues access for the database.
+ * Interface Access The singleton object that both manages and queues access for
+ * the database.
  * 
  * It is synchronized for relatively safe multithreading.
  * 
- * Instance of this is protected with AccessImpl,
- * use AccessImpl.getInstance() to access.
+ * Instance of this is protected with AccessImpl, use AccessImpl.getInstance()
+ * to access.
  * 
  * @author Antti Knutas
  * 
  */
 public interface Access {
 
-  //
-  // Fields
-  //
-	
+    /**
+     * Orders the persistance manager to close connections and finish.
+     */
+    public void finalize();
 
-  
-  //
-  // Methods
-  //
-  
-  /**
-   * Orders the persistance manager to close connections and finish.
-   */
-  public void finalize(  );
+    /**
+     * Takes a string as a query, and returns the result as an array of objects.
+     * 
+     * @return List Result list of objects.
+     * @param querystring Query, formatted by QueryBuilder.
+     */
+    @SuppressWarnings("unchecked")
+    public List query(String querystring);
 
+    /**
+     * Persists a single object.
+     * 
+     * @return int Success status.
+     * @param object The persisted object.    
+     */
+    public int commitOne(Object object);
 
-  /**
-   * Takes a string as a query, and returns the result as an array of objects.
-   * @return       List Result list of objects.
-   * @param        querystring Query, formatted by QueryBuilder.
-   */
-  @SuppressWarnings("unchecked")
-public List query( String querystring );
+    /**
+     * Removes a single object from the persistence graph.
+     * 
+     * @return int Success status.
+     * @param object The persisted object.   
+     */
+    public int removeOne(Object object);
 
-	/**
-	 * Persists a single object.
-	 * @return       int Success status.
-	 * @param        object The persisted object.
-	 */
-  public int commitOne( Object object );
-  
-	/**
-	 * Removes a single object from the persistence graph.
-	 * @return       int Success status.
-	 * @param        object The persisted object.
-	 */
-public int removeOne( Object object );
-
-	/**
-	 * Persists the objects in the list.
-	 * @return       int Success status.
-	 * @param        objects List of objects to be persisted.
-	 */
-  public int commit( List objects );
-
+    /**
+     * Persists the objects in the list.
+     * 
+     * @return int Success status.
+     * @param objects List of objects to be persisted.
+     */
+    public int commit(List objects);
 
 }
--- src/database/AccessImpl.java
+++ src/database/AccessImpl.java
@@ -11,192 +11,152 @@ import java.util.*;
  */
 public class AccessImpl implements Access {
 
-	//
-	// Fields
-	//
+    private static database.Access instance;
+    private PersistenceManager pm;
+    private PersistenceManagerFactory pmf;
 
-	private static database.Access instance;
-	private PersistenceManager pm;
-	private PersistenceManagerFactory pmf;
+    protected AccessImpl() {
+	initialize();
+    };
 
-	//
-	// Constructors
-	//
-	protected AccessImpl () {
-		initialize();
-	};
+    public void finalize() {
+	pm.close();
+	instance = null;
+    }
 
-	public void finalize ()
-	{
-		pm.close();
-		instance=null;
+    /**
+     * Takes a string as a query, and returns the result as an array of objects.
+     * 
+     * @return List Result list of objects.
+     * @param querystring Query, formatted by QueryBuilder. 
+     */
+    @SuppressWarnings("unchecked")
+    public synchronized List query(String querystring) {
+	Transaction tx = pm.currentTransaction();
+	Query q = pm.newQuery(querystring);
+	List results = null;
+
+	try {
+	    // Beginning transaction
+	    tx.begin();
+	    results = (List) q.execute();
+	    tx.commit();
+	} catch (Exception e) {
+	    e.printStackTrace();
+	} finally {
+	    if (tx.isActive()) {
+		tx.rollback();
+	    }
 	}
+	return results;
+    }
 
-	//
-	// Methods
-	//
-
-	/**
-	 * Takes a string as a query, and returns the result as an array of objects.
-	 * @return       List Result list of objects.
-	 * @param        querystring Query, formatted by QueryBuilder.
-	 */
-	@SuppressWarnings("unchecked")
-	public synchronized List query( String querystring )
-	{
-		Transaction tx=pm.currentTransaction();
-		Query q=pm.newQuery(querystring);
-		List results = null;
-
-		try
-		{
-			//Beginning transaction
-			tx.begin();
-			results = (List)q.execute();
-			tx.commit();
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-		finally
-		{
-			if (tx.isActive())
-			{
-				tx.rollback();
-			}
-		}
-		return results;
+    /**
+     * Persists a single object.
+     * 
+     * @return int Success status.
+     * @param object The persisted object.
+     */
+    public synchronized int commitOne(Object object) {
+	int returnvalue = 1;
+	Transaction tx = pm.currentTransaction();
+	try {
+	    // Beginning transaction
+	    tx.begin();
+	    pm.makePersistent(object);
+	    tx.commit();
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    returnvalue = 0;
+	} finally {
+	    if (tx.isActive()) {
+		tx.rollback();
+	    }
 	}
+	return returnvalue;
+    }
 
-	/**
-	 * Persists a single object.
-	 * @return       int Success status.
-	 * @param        object The persisted object.
-	 */
-	public synchronized int commitOne( Object object )
-	{
-		int returnvalue = 1;
-		Transaction tx=pm.currentTransaction();
-		try
-		{
-			//Beginning transaction
-			tx.begin();
-			pm.makePersistent(object);
-			tx.commit();
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-			returnvalue = 0;	
-		}
-		finally
-		{
-			if (tx.isActive())
-			{
-				tx.rollback();
-			}
-		}
-		return returnvalue;
+    /**
+     * Persists the objects in the list.
+     * 
+     * @return int Success status.
+     * @param objects List of objects to be persisted.
+     */
+    @SuppressWarnings("unchecked")
+    public synchronized int commit(List objects) {
+	int returnvalue = 1;
+	Transaction tx = pm.currentTransaction();
+	try {
+	    // Beginning transaction
+	    tx.begin();
+	    pm.makePersistentAll(objects);
+	    tx.commit();
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    returnvalue = 0;
+	} finally {
+	    if (tx.isActive()) {
+		tx.rollback();
+	    }
 	}
+	return returnvalue;
+    }
 
-
-	/**
-	 * Persists the objects in the list.
-	 * @return       int Success status.
-	 * @param        objects List of objects to be persisted.
-	 */
-	@SuppressWarnings("unchecked")
-	public synchronized int commit( List objects )
-	{
-		int returnvalue = 1;
-		Transaction tx=pm.currentTransaction();
-		try
-		{
-			//Beginning transaction
-			tx.begin();
-			pm.makePersistentAll(objects);
-			tx.commit();
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-			returnvalue = 0;	
-		}
-		finally
-		{
-			if (tx.isActive())
-			{
-				tx.rollback();
-			}
-		}
-		return returnvalue;
+    /**
+     * Returns 1 in case of a successful initialization.
+     * 
+     * @return int Something remarkable
+     */
+    private synchronized int initialize() {
+	// Trying to create the persistance manager
+	try {
+	    pmf = JDOHelper
+	    .getPersistenceManagerFactory("datanucleus.properties");
+	    pm = pmf.getPersistenceManager();
+	    pm.getFetchPlan().setMaxFetchDepth(-1);
+	    pm.setDetachAllOnCommit(true);
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    return 0;
 	}
+	return 1;
+    }
 
-
-	/**
-	 * Returns 1 in case of a successful initialization.
-	 * @return       int
-	 */
-	private synchronized int initialize(  )
-	{
-		//Trying to create the persistance manager
-		try {
-			pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
-			pm = pmf.getPersistenceManager();
-			pm.getFetchPlan().setMaxFetchDepth(-1);
-			pm.setDetachAllOnCommit(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return 0;
-		}
-		return 1;
+    /**
+     * Returns the singleton object reference.
+     * 
+     * @return Database.Access Singleton object reference
+     */
+    public synchronized static database.Access getInstance() {
+	if (instance == null) {
+	    instance = new AccessImpl();
 	}
+	return instance;
+    }
 
-
-	/**
-	 * Returns the singleton object reference.
-	 * @return       Database.Access
-	 */
-	public synchronized static database.Access getInstance(   )
-	{
-		if (instance==null)
-		{
-			instance = new AccessImpl();
-		}
-		return instance;
+    /**
+     * Removes a single object from the persistence graph.
+     * 
+     * @return int Success status.
+     * @param object The persisted object.
+     */
+    public synchronized int removeOne(Object object) {
+	int returnvalue = 1;
+	Transaction tx = pm.currentTransaction();
+	try {
+	    // Beginning transaction
+	    tx.begin();
+	    pm.deletePersistent(object);
+	    tx.commit();
+	} catch (Exception e) {
+	    e.printStackTrace();
+	    returnvalue = 0;
+	} finally {
+	    if (tx.isActive()) {
+		tx.rollback();
+	    }
 	}
-
-	/**
-	 * Removes a single object from the persistence graph.
-	 * @return       int Success status.
-	 * @param        object The persisted object.
-	 */
-	public synchronized int removeOne( Object object )
-	{
-		int returnvalue = 1;
-		Transaction tx=pm.currentTransaction();
-		try
-		{
-			//Beginning transaction
-			tx.begin();
-			pm.deletePersistent(object);
-			tx.commit();
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-			returnvalue = 0;	
-		}
-		finally
-		{
-			if (tx.isActive())
-			{
-				tx.rollback();
-			}
-		}
-		return returnvalue;
-	}
-
+	return returnvalue;
+    }
 
 }
