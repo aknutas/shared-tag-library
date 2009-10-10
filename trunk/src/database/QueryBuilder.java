20ac6003bef47c028e4a7cac3c7999e6581d8503
shingis <shingis@e62d969a-9d79-11de-8ef5-057bcddc142d>
2009-10-10 01:41:08 +0000

    Changed code to match project coding standards.

--- trunk/src/database/QueryBuilder.java
+++ trunk/src/database/QueryBuilder.java
@@ -5,64 +5,61 @@ import java.util.*;
 import data.Bookshelf;
 import network.Control;
 
-
 /**
- * Interface QueryBuilder
- * This static class creates a helper object for managing database query input into
- * several different formats. Access methods created as needed.
+ * Interface QueryBuilder This static class creates a helper object for managing
+ * database query input into several different formats. Access methods created
+ * as needed.
  * 
  * @author Antti Knutas
  * 
  */
 public interface QueryBuilder {
 
-  //
-  // Fields
-  //
+    /**
+     * Returns all bookshelves with a book object with this specific title.
+     * 
+     * @return List A list of shelves.
+     * @param booktitle Book title.
+     *          
+     * @depreciated
+     */
+    public List<Bookshelf> shelfSearchByBookName(String booktitle);
 
-  
-  //
-  // Methods
-  //
+    /**
+     * Returns all bookshelves with this specific property.
+     * 
+     * @return List A list of shelves.
+     * @param property Property name.
+     * @param value Property value.
+     *            
+     * @depreciated
+     */
+    public List<Bookshelf> shelfSearchByProperty(String property, String value);
 
-  /**
-   * Returns all bookshelves with a book object with this specific title.
-   * DOES NOT WORK YET
-   * @return       List A list of shelves.
-   * @param        booktitle Book title.
-   */
-  public List<Bookshelf> shelfSearchByBookName( String booktitle );
-  
-  /**
-   * Returns all bookshelves with this specific property.
-   * DOES NOT WORK YET
-   * @return       List A list of shelves.
-   * @param        property Property name.
-   * @param        value Property value.
-   */
-  public List<Bookshelf> shelfSearchByProperty( String property, String value );
-  
-  /**
-   * Returns all bookshelves.
-   * @return       List A list of shelves.
-   */
-  public List<Bookshelf> shelfList(   );
+    /**
+     * Returns all bookshelves.
+     * 
+     * @return List A list of shelves.
+     */
+    public List<Bookshelf> shelfList();
 
-  /**
-   * Stores new shelf, or updates the database with the changes. Takes the persisted shelf object as a parameter.
-   * @return       int Success status.
-   * @param        shelf The shelf to be stored or updated.
-   */
-  public int shelfStore( Bookshelf shelf );
+    /**
+     * Stores new shelf, or updates the database with the changes. Takes the
+     * persisted shelf object as a parameter.
+     * 
+     * @return int Success status.
+     * @param shelf The shelf to be stored or updated.
+     *            
+     */
+    public int shelfStore(Bookshelf shelf);
 
-
-  /**
-   * Searches for, and removes an instance of this bookshelf from the database.
-   * Returns integer value of the success status.
-   * @return       int
-   * @param        shelf
-   */
-  public int shelfRemove( Bookshelf shelf );
-
+    /**
+     * Searches for, and removes an instance of this bookshelf from the
+     * database. Returns integer value of the success status.
+     * 
+     * @return int
+     * @param shelf
+     */
+    public int shelfRemove(Bookshelf shelf);
 
 }
--- trunk/src/database/QueryBuilderImpl.java
+++ trunk/src/database/QueryBuilderImpl.java
@@ -4,104 +4,97 @@ import java.util.*;
 
 import data.Bookshelf;
 
-
 /**
- * Class QueryBuilderImpl
- * QueryBuilder implementation.
+ * Class QueryBuilderImpl QueryBuilder implementation.
  * 
  * @author Antti Knutas
  * 
  */
 public final class QueryBuilderImpl implements QueryBuilder {
 
-  //
-  // Fields
-  //
+    public QueryBuilderImpl() {
+    };
 
-  
-  //
-  // Constructors
-  //
-  public QueryBuilderImpl () { };
-  
-  //
-  // Methods
-  //
+    /**
+     * Returns all bookshelves with a book object with this specific title.
+     * 
+     * @return List A list of shelves.
+     * @param String Book title.
+     *            
+     * @depreciated
+     */
+    @SuppressWarnings("unchecked")
+    public List<Bookshelf> shelfSearchByBookName(String booktitle) {
+	Access db = AccessImpl.getInstance();
+	String querystring = "   SELECT FROM data.VirtualBookshelf WHERE (bookshelf.contains(book) book.properties.containsKey(key) && key == 'title' && bookshelf.book.properties.containsValue(value) && value == '"
+	    + booktitle
+	    + "') VARIABLES data.VirtualBook book; String key; String value;";
 
-  /**
-   * Returns all bookshelves with a book object with this specific title.
-   * DOES NOT WORK YET
-   * @return       List A list of shelves.
-   * @param        booktitle Book title.
-   */
-  @SuppressWarnings("unchecked") 
-public List<Bookshelf> shelfSearchByBookName( String booktitle )
-  {
-	  Access db = AccessImpl.getInstance();
-	  String querystring = "SELECT FROM data.VirtualBookshelf WHERE (bookshelf.contains(book) book.properties.containsKey(key) && key == 'title' && bookshelf.book.properties.containsValue(value) && value == '" + booktitle + "') VARIABLES data.VirtualBook book; String key; String value;";
-	
-	  List<Bookshelf> returnlist = db.query(querystring);
-	  
-	  return returnlist;
-  }
-  
-  /**
-   * Returns all bookshelves with this specific property.
-   * DOES NOT WORK YET
-   * @return       List A list of shelves.
-   * @param        booktitle Book title.
-   */
-  @SuppressWarnings("unchecked")
-public List<Bookshelf> shelfSearchByProperty( String property, String value )
-  {
-	  Access db = AccessImpl.getInstance();
-	  String querystring = "SELECT FROM data.VirtualBookshelf WHERE (book.properties.containsEntry(key, value) && key = '" + property + "' && value = '" + value + "')";
-	
-	  List<Bookshelf> returnlist = db.query(querystring);
-	  
-	  return returnlist;
-  }
-  
-  /**
-   * Returns all bookshelves.
-   * @return       Array A list of shelves.
-   */
-  @SuppressWarnings("unchecked")
-public List<Bookshelf> shelfList(   )
-  {
-	  Access db = AccessImpl.getInstance();
-	  String querystring = "SELECT FROM data.VirtualBookshelf";
-	
-	  List<Bookshelf> returnlist = (List<Bookshelf>)db.query(querystring);
-	  
-	  return returnlist;
-  }
+	List<Bookshelf> returnlist = db.query(querystring);
 
-  /**
-   * Stores new shelf, or updates the database with the changes. Takes the persisted shelf object as a parameter.
-   * @return       int Success status.
-   * @param        shelf The shelf to be stored or updated.
-   */
-  public int shelfStore( Bookshelf shelf )
-  {
+	return returnlist;
+    }
+
+    /**
+     * Returns all bookshelves with this specific property.
+     * 
+     * @return List A list of shelves.
+     * @param booktitle Book title.
+     *            
+     * @depreciated
+     */
+    @SuppressWarnings("unchecked")
+    public List<Bookshelf> shelfSearchByProperty( String property, String value )
+    { 
+	Access db = AccessImpl.getInstance();
+	String querystring = "SELECT FROM data.VirtualBookshelf WHERE properties.containsKey(key) && key == \"" + property + "\" && properties.containsValue(value) && value == \"" + value + "\" VARIABLES String key; String value;";
+
+	List<Bookshelf> returnlist = db.query(querystring);
+
+	return returnlist;
+    }
+
+    /**
+     * Returns all bookshelves.
+     * 
+     * @return Array A list of shelves.
+     */
+    @SuppressWarnings("unchecked")
+    public List<Bookshelf> shelfList() {
+	Access db = AccessImpl.getInstance();
+	String querystring = "SELECT FROM data.VirtualBookshelf";
+
+	List<Bookshelf> returnlist = (List<Bookshelf>) db.query(querystring);
+
+	return returnlist;
+    }
+
+    /**
+     * Stores new shelf, or updates the database with the changes. Takes the
+     * persisted shelf object as a parameter.
+     * 
+     * @return int Success status.
+     * @param shelf The shelf to be stored or updated.   
+     */
+    public int shelfStore(Bookshelf shelf) {
 	Access db = AccessImpl.getInstance();
 	int returnvalue = db.commitOne(shelf);
-	
-	return returnvalue;
-  }
 
-  /**
-   * Searches for, and removes an instance of this bookshelf from the database.
-   * Returns the success status.
-   * @return       boolean
-   * @param        shelf
-   */
-  public int shelfRemove( Bookshelf shelf )
-  {
+	return returnvalue;
+    }
+
+    /**
+     * Searches for, and removes an instance of this bookshelf from the
+     * database. Returns the success status.
+     * 
+     * @return boolean
+     * @param shelf
+     */
+    public int shelfRemove(Bookshelf shelf) {
 	Access db = AccessImpl.getInstance();
 	int returnvalue = db.removeOne(shelf);
-	
+
 	return returnvalue;
-  }
-  
+    }
+
 }
