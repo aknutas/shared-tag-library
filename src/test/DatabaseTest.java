3f444216f5eb7581316fb354b717f1e268efbd78
shingis <shingis@e62d969a-9d79-11de-8ef5-057bcddc142d>
2009-10-11 06:16:06 +0000

    Updated and commented the database unit test.

--- src/test/DatabaseTest.java
+++ src/test/DatabaseTest.java
@@ -12,59 +12,58 @@ import javax.jdo.Transaction;
 
 public class DatabaseTest {
 
-	/**
-	 * Note: The unit tests work only on a completely empty database. Clutter will show up as errors.
-	 */
-	public static void main(String[] args) {
-		QueryBuilder qb = new QueryBuilderImpl();
-		Book book;
-		Bookshelf shelf;
-		List<Bookshelf> returnlist;
-		Bookshelf testshelf;
+    /**
+     * Note: The unit tests work only on a completely empty database. Clutter
+     * will show up as errors.
+     */
+    public static void main(String[] args) {
+	//Initializing variables
+	QueryBuilder qb = new QueryBuilderImpl();
+	Book book;
+	Bookshelf shelf;
+	List<Bookshelf> returnlist;
+	Bookshelf testshelf;
 
-		shelf = new VirtualBookshelf("");
-		shelf.setProperty("name", "Plop");
-		book = new VirtualBook("Diiba", "Daaba");
-		shelf.insert(book);
+	//Creating new shelf
+	shelf = new VirtualBookshelf("");
+	shelf.setProperty("name", "Plop");
+	book = new VirtualBook("Diiba", "Daaba");
+	shelf.insert(book);
+	//Here is where we persist it
+	qb.shelfStore(shelf);
 
-		/*
-		qb.shelfStore(shelf);
-		returnlist = qb.shelfSearchByBookName("Diiba");
-		testshelf = (Bookshelf)returnlist.get(0);
-		
-		assert testshelf.getProperty("name").equals(shelf.getProperty("name"));
-		assert returnlist.size()==1;
-		*/
-		
-		for(int i=0; i<100; i++)
-		{
-			book = new VirtualBook(Integer.toString(i), Integer.toString(i));
-			for(int j=0; j<5; j++)
-			{
-				book.tag(Integer.toString(j));
-			}
-			shelf.insert(book);
-		}
-		
-		qb.shelfStore(shelf);
-		returnlist = qb.shelfList();
-		testshelf = (Bookshelf)returnlist.get(0);
-		
-		assert testshelf.getProperty("name").equals(shelf.getProperty("name"));
-		assert testshelf.size()==101;
-		assert (returnlist.size()==1);
-		
-		Iterator<Book> e = shelf.enumerate();
-		
-		while(e.hasNext())
-		{
-			System.out.println(e.next().getProperty("title"));
-		}
-		
-		/*
-		returnlist = qb.shelfSearchByBookName("Daaba");
-		assert (returnlist.size()==0);
-		*/
+	//First test query
+	returnlist = qb.shelfSearchByProperty("name", "Plop");
+	testshelf = (Bookshelf) returnlist.get(0);
+	assert testshelf.getProperty("name").equals("Plop");
+
+	// Second test query
+	returnlist = qb.shelfSearchByBookName("Diiba");
+	testshelf = (Bookshelf) returnlist.get(0);
+	assert testshelf.getProperty("name").equals(shelf.getProperty("name"));
+	assert returnlist.size() == 1;
+
+	// Filling with more content
+	for (int i = 0; i < 100; i++) {
+	    book = new VirtualBook(Integer.toString(i), Integer.toString(i));
+	    for (int j = 0; j < 5; j++) {
+		book.tag(Integer.toString(j));
+	    }
+	    shelf.insert(book);
 	}
 
+	// Updating the database with updated object
+	qb.shelfStore(shelf);
+
+	// Fetching new results
+	returnlist = qb.shelfList();
+	testshelf = (Bookshelf) returnlist.get(0);
+
+	assert testshelf.getProperty("name").equals(shelf.getProperty("name"));
+	assert testshelf.size() == 101;
+	assert (returnlist.size() == 1);
+
+	returnlist = qb.shelfSearchByBookName("Daaba");
+	assert (returnlist.size() == 0);
+    }
 }
