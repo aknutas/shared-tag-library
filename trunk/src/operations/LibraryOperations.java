33623e9c52abfcc82b83d820f6ed4c37c1959ed4
ashmore.fire.bringer <ashmore.fire.bringer@e62d969a-9d79-11de-8ef5-057bcddc142d>
2009-10-23 17:27:22 +0000

    Added getTags and getProperties to LibraryOperations.

--- trunk/src/operations/LibraryOperations.java
+++ trunk/src/operations/LibraryOperations.java
@@ -1,8 +1,49 @@
 package operations;
 
 import data.*;
-import java.util.Iterator;
+
+import java.util.*;
 
 public abstract class LibraryOperations {
 
+	public static Iterator<Map.Entry<String, Integer>> getTags(Library lib){
+		
+		Iterator<Book> master = lib.getMasterShelf().enumerate();
+		Map<String, Integer> allTags = new HashMap<String, Integer>();
+		
+		while (master.hasNext()){
+			Book current = master.next();
+			Iterator<Map.Entry<String, Integer>> tags = current.enumerateTags();
+			while (tags.hasNext()){
+				Map.Entry<String,Integer> tag = tags.next();
+				if (allTags.containsKey(tag.getKey())) {
+					Integer old = allTags.remove(tag.getKey());
+					allTags.put(tag.getKey(), old + tag.getValue());
+				}
+				else
+					allTags.put(tag.getKey(), tag.getValue());
+			}
+			
+		}
+		
+		return allTags.entrySet().iterator();
+	}
+	
+	public static Iterator<Map.Entry<String, String>> getProperties(Library lib){
+		
+		Iterator<Book> master = lib.getMasterShelf().enumerate();
+		Set<Map.Entry<String, String>> allProperties = new TreeSet<Map.Entry<String, String>>();
+		
+		while (master.hasNext()){
+			Book current = master.next();
+			Iterator<Map.Entry<String, String>> props = current.enumerateProperties();
+			while (props.hasNext()){
+				Map.Entry<String, String> prop = props.next();
+				allProperties.add(prop);
+			}
+		}
+		
+		return allProperties.iterator();
+		
+	}
 }
