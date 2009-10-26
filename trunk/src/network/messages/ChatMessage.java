bc5abe36a2087feb8b5b4bf3c2c369735d195007
shingis <shingis@e62d969a-9d79-11de-8ef5-057bcddc142d>
2009-10-26 16:32:11 +0000

    Added more basic details of the network package, and few messages.

---
+++ trunk/src/network/messages/ChatMessage.java
@@ -0,0 +1,34 @@
+package network.messages;
+
+import java.io.Serializable;
+
+/**
+ * A simple chat message.
+ * 
+ * @author Antti Knutas
+ * 
+ */
+public class ChatMessage extends Message implements Serializable {
+
+    private static final long serialVersionUID = -6448803844411292176L;
+    private String message;
+
+    /**
+     * Private default constructor. Do not create empty messages.
+     */
+    @SuppressWarnings("unused")
+    private ChatMessage() {
+
+    }
+
+    public ChatMessage(String message) {
+	if (message == null)
+	    this.message = "Hi";
+	else
+	    this.message = message;
+    }
+
+    public String GetMessage() {
+	return message;
+    }
+}
---
+++ trunk/src/network/messages/Message.java
@@ -0,0 +1,14 @@
+package network.messages;
+
+import java.io.Serializable;
+
+/**
+ * Message abstract class.
+ * 
+ * @author Antti Knutas
+ * 
+ */
+@SuppressWarnings("serial")
+public abstract class Message implements Serializable {
+
+}
