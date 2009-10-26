66feac174f84dd92424614cf0a029f8d59c51b19
shingis <shingis@e62d969a-9d79-11de-8ef5-057bcddc142d>
2009-10-26 16:44:13 +0000

    Skeletal structures

--- src/network/Thread.java
+++ src/network/Thread.java
@@ -1,37 +1,46 @@
 package network;
 
+import java.net.Socket;
 import java.util.*;
-
+import database.QueryBuilder;
+import database.QueryBuilderImpl;
 
 /**
  * Class Thread
- * 
- * @author Antti Knutas
- * 
  */
 public class Thread {
 
-  //
-  // Fields
-  //
+    private int status;
+    private Socket s;
+    private Communication comm = new CommunicationImpl();
+    private QueryBuilder qb = new QueryBuilderImpl();
 
-  
-  //
-  // Constructors
-  //
-  public Thread () { };
-  
-  //
-  // Methods
-  //
+    public Thread() {
+    };
 
+    /**
+     * Set the value of status
+     * 
+     * @param newVar
+     *            the new value of status
+     */
+    private void setStatus(int newVar) {
+	status = newVar;
+    }
 
-  //
-  // Accessor methods
-  //
+    /**
+     * Get the value of status
+     * 
+     * @return the value of status
+     */
+    private int getStatus() {
+	return status;
+    }
 
-  //
-  // Other methods
-  //
+    public void sendMsg() {
+    }
+
+    public void getMsg() {
+    }
 
 }
