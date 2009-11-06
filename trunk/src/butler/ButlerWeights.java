b7bee462e0c9aa7b83fbfb601bc11ebeaa7882fb
ashmore.fire.bringer <ashmore.fire.bringer@e62d969a-9d79-11de-8ef5-057bcddc142d>
2009-11-06 18:40:48 +0000

    Wrapper class for the 3 matrixes of the Butler layers' weights to allow for easy storage in the DB.

---
+++ trunk/src/butler/ButlerWeights.java
@@ -0,0 +1,49 @@
+package butler;
+
+import org.joone.engine.Matrix;
+import java.util.Date;
+
+public class ButlerWeights {
+
+	Matrix inputs;
+	Matrix hidden;
+	Matrix outputs;
+	Long timestamp;
+	
+	public ButlerWeights(Matrix in, Matrix hid, Matrix out){
+		inputs = in;
+		hidden = hid;
+		outputs = out;
+		Date time = new Date();
+		timestamp = time.getTime();
+		
+	}
+	
+	public Matrix getInputWeights(){
+		return inputs;
+	}
+	
+	public Matrix getHiddenWeights(){
+		return hidden;
+	}
+	
+	public Matrix getOutputWeights(){
+		return outputs;
+	}
+	
+	public void setInputWeights(Matrix in){
+		inputs = in;
+	}
+	
+	public void setHiddenWeights(Matrix hid){
+		hidden = hid;
+	}
+	
+	public void setOutputWeights(Matrix out){
+		outputs = out;
+	}
+	
+	public Long getTimestamp(){
+		return timestamp;
+	}
+}
