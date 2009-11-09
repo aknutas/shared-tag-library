b76d926844d3516a91fdcceef7e004c9e316e7a3
ashmore.fire.bringer <ashmore.fire.bringer@e62d969a-9d79-11de-8ef5-057bcddc142d>
2009-11-09 18:04:10 +0000

    Tagged 2 more sample books.

--- src/scripts/CreateTestData.java
+++ src/scripts/CreateTestData.java
@@ -23,8 +23,91 @@ public class CreateTestData {
 		((PersistentLibrary)library).saveBookshelf(shelf1);
 		
 		VirtualBookshelf shelf2 = new VirtualBookshelf("Classic Cannon");
-		shelf2.insert(new VirtualBook("The Old Man and the Sea", "Hemingway"));
-		shelf2.insert(new VirtualBook("Catcher in the Rye", "Salinger"));
+		VirtualBook oldMan = new VirtualBook("The Old Man and the Sea", "Hemingway");
+		
+
+		//tagging Old Man and the Sea
+		String[] oldManTags = {"classic literature","ernest hemingway",
+				"american literature","classics","hemingway","fiction",
+				"classic","book","literature","novel",
+				"20th century american fiction","fishing","20th century",
+				"marlin","united states","1950s",
+				"20th century american literature","20th century lit","american",
+				"american fiction","assigned reading","author suicides","awesome",
+				"bait","book recommendations","bookshop","caribbean coctail",
+				"carlos castaneda","classic fiction","classic lit",
+				"dead souls and dark alleys","deep-sea fishing","defeat",
+				"determination","dont look forward","english","faith",
+				"favorite books","fear","fiction classics","fish story",
+				"fisherman","fishing line","fishy story","gift for sister",
+				"gift from jeanne","great books","harpoon","history",
+				"hummingbird god","juvenile writing","literature nobel prize winners",
+				"american literature","my books","nature writing","nautical",
+				"nobel prize in literature","novella","old west","overrated classics",
+				"philosophy","picaresque","pile of crap","pippi longstocking",
+				"pretentious garbage","pulitzer - fiction","school books","sharks",
+				"short fiction","spain - portugal - latin america","stars i like it",
+				"timeless literature","trips and journeys","veronica diaz-reinhagen",
+				"veronica martino"};
+		
+		int[] oldManWeights = {51,50,34,29,25,24,23,12,12,6,5,3,2,2,1,1,
+				1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+				1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+		
+		for (int i = 0; i < oldManTags.length; ++i){
+			int num = oldManWeights[i];
+			while (num > 0)
+				oldMan.tag(oldManTags[i]);
+		}
+		
+		shelf2.insert(oldMan);
+		
+		VirtualBook rye = new VirtualBook("Catcher in the Rye", "Salinger");
+		
+		String[] ryeTags = {"coming of age (98)","classic literature (93)","jd salinger (66)",
+				"classic fiction (56)","catcher in the rye (47)","teen (47)","classic (44)",
+				"holden caulfield (20)","fiction (17)","20th century american fiction (15)",
+				"literature (13)","classics (6)","salinger (6)","9 99boycott (4)",
+				"the lost episodes of beatie scareli (4)","american literature (3)","book (3)",
+				"book recommendations (3)","ginnetta correli (3)","black comedy (2)","holden (2)",
+				"literary fiction (2)","novel (2)","outsider writers (2)","rye (2)",
+				"the catcher in the rye (2)","waste of time (2)","0316769177 (1)",
+				"9780316769174 (1)","a space between (1)","ackley (1)","add (1)","angst (1)",
+				"anxiety (1)","	audrey michelle (1)","bazhe (1)","best (1)","bildungsroman (1)",
+				"carl (1)","carol rose (1)","catche (1)","catcher (1)","catcher rye (1)",
+				"classic american novel (1)","classic lit (1)","classic novel (1)",
+				"come out of the cave (1)","communication (1)","controversial (1)",
+				"cynicism (1)","depression (1)","disillusionment (1)","ed roberts (1)",
+				"emo catcher in the rye teenager (1)","fiction that reads like a biography (1)",
+				"flit (1)","generational issues (1)","glenn thater (1)","grief (1)","historical fiction (1)",
+				"holden caufield (1)","holden caufiled salingner (1)","homeschooling (1)",
+				"horrible literary masterpieces (1)","humor (1)","indie (1)","innocence (1)",
+				"j d salinger (1)","jane (1)","jd (1)","john lennon (1)","john lennon murderer (1)",
+				"john lennon s murderer (1)","late writer (1)","lit american (1)","literary (1)",
+				"masculinity (1)","mental illness (1)","must reads (1)","narrator (1)","new age (1)",
+				"new york city (1)","new york stories (1)","nick harris (1)","over-rated (1)",
+				"own book (1)","period piece (1)","personal psychoanalysis by holden (1)","phoebe (1)",
+				"picaresque (1)","piedmont reading club (1)","posttraumatic stress disorder (1)",
+				"pulitzer - drama (1)","really do (1)","rebellion (1)","red book (1)","school books (1)",
+				"stradlater (1)","style (1)","suspense (1)","swear word in every sentence (1)",
+				"teen angst (1)","teen literature (1)","teenage drinking (1)","the bathsheba deadline (1)",
+				"the catcher in the rye taiwanese view (1)","the days of the bitter end (1)",
+				"the fallen angle (1)","the gateway (1)","the last panther (1)","too much profanity in a book (1)",
+				"ugh (1)","useful (1)","voice (1)","ya fiction (1)","young boy (1)"};
+		
+		int[] ryeWeights = {98,93,66,56,47,47,44,20,17,15,13,6,6,4,4,3,3,3,3,2,2,2,2,2,2,
+				2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+				1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+				1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+		
+		for (int i = 0; i < ryeTags.length; ++i){
+			int num = ryeWeights[i];
+			while (num > 0)
+				oldMan.tag(ryeTags[i]);
+		}
+		
+		shelf2.insert(rye);
+		
 		shelf2.insert(new VirtualBook("Grapes of Wrath", "Steinbeck"));
 		shelf2.insert(new VirtualBook("The Adventures of Huckleberry Fin", "Twain"));
 		shelf2.insert(new VirtualBook("The Great Gatsby", "Fitzgeralrd"));
@@ -32,6 +115,7 @@ public class CreateTestData {
 		shelf2.insert(new VirtualBook("Moby Dick", "Melville"));
 		shelf2.insert(new VirtualBook("Jane Eyre", "Bront"));
 		shelf2.insert(new VirtualBook("For Whom the Bell Tolls", "Hemingway"));
+		
 		((PersistentLibrary)library).saveBookshelf(shelf2);
 		
 		VirtualBookshelf shelf3 = new VirtualBookshelf("Seussville");
