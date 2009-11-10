821c87498c63726387236bfedb6eb26acd7e4402
shingis <shingis@e62d969a-9d79-11de-8ef5-057bcddc142d>
2009-11-10 02:59:32 +0000

    Adjusted messageHandler timer to be more reasonable now that the debug flood is gone.

--- trunk/src/main/Root.java
+++ trunk/src/main/Root.java
@@ -79,7 +79,7 @@ public class Root extends JFrame {
 		super();
 		//This is where message trigger handler gets created and started up.
 		msgTrigger = new MsgTrigger();
-		msgTimer = new javax.swing.Timer(5000, msgTrigger);
+		msgTimer = new javax.swing.Timer(1000, msgTrigger);
 		msgTimer.setInitialDelay(2000);
 		msgTimer.start();
 		initialize();
