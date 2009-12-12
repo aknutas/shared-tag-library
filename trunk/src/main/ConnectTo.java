1ecca3e90786811c46bebe1f94f174a0fcb4c46e
pranspach <pranspach@e62d969a-9d79-11de-8ef5-057bcddc142d>
2009-12-12 02:32:07 +0000

    connect/disconnect

--- trunk/src/main/ConnectTo.java
+++ trunk/src/main/ConnectTo.java
@@ -8,10 +8,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.io.IOException;
-import java.net.UnknownHostException;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -22,7 +22,6 @@ import javax.swing.UnsupportedLookAndFeelException;
 import org.jvnet.substance.skin.SubstanceBusinessBlackSteelLookAndFeel;
 
 import controller.Controller;
-import data.RemoteObjectException;
 
 public class ConnectTo extends JDialog {
 
@@ -36,6 +35,8 @@ public class ConnectTo extends JDialog {
     private JLabel jLabel = null;
     private JLabel jLabel1 = null;
     private TreeView tree = null;
+    private JLabel jLabel2 = null;
+    private JComboBox connectionList = null;
 
     /**
      * @param owner
@@ -115,11 +116,20 @@ public class ConnectTo extends JDialog {
 		public void actionPerformed(ActionEvent event) {
 
 		    try {
-			control.addConnection(addressField.getText(), ipAlias
+			if (addressField.getText().equals("")){
+			    control.connect((String)connectionList.getSelectedItem());
+			}
+			else {
+			    control.addConnection(addressField.getText(), ipAlias
 				.getText());
-			control.connect(ipAlias.getText());
-			tree.refresh();
-		    }catch (IllegalArgumentException e) {
+				control.connect(ipAlias.getText());
+			}
+			
+			//ChooseBookshelves dialog = new ChooseBookshelves(thisClass, control,
+			//	    treeView, /*lib*/);
+			 //   dialog.setVisible(true);
+			// tree.refresh();
+		    } catch (IllegalArgumentException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		    } catch (NullPointerException e) {
@@ -148,9 +158,22 @@ public class ConnectTo extends JDialog {
 			.println("Cannot set new Theme for Java Look and Feel.");
 	    }
 
+	    GridBagConstraints gridBagConstraints41 = new GridBagConstraints();
+	    gridBagConstraints41.fill = GridBagConstraints.HORIZONTAL;
+	    gridBagConstraints41.gridy = 0;
+	    gridBagConstraints41.gridwidth = 2;
+	    gridBagConstraints41.gridx = 1;
+	    gridBagConstraints41.insets = new Insets(2, 2, 2, 2);
+	    GridBagConstraints gridBagConstraints31 = new GridBagConstraints();
+	    gridBagConstraints31.gridx = 0;
+	    gridBagConstraints31.gridy = 0;
+	    gridBagConstraints31.insets = new Insets(2, 2, 2, 2);
+	    gridBagConstraints31.anchor = GridBagConstraints.EAST;
+	    jLabel2 = new JLabel();
+	    jLabel2.setText("Past Connections: ");
 	    GridBagConstraints gridBagConstraints21 = new GridBagConstraints();
 	    gridBagConstraints21.gridx = 0;
-	    gridBagConstraints21.gridy = 0;
+	    gridBagConstraints21.gridy = 1;
 	    gridBagConstraints21.anchor = GridBagConstraints.EAST;
 	    gridBagConstraints21.insets = new Insets(2, 2, 2, 2);
 	    jLabel1 = new JLabel();
@@ -158,27 +181,28 @@ public class ConnectTo extends JDialog {
 
 	    GridBagConstraints gridBagConstraints4 = new GridBagConstraints();
 	    gridBagConstraints4.gridx = 2;
-	    gridBagConstraints4.gridy = 2;
+	    gridBagConstraints4.gridy = 3;
 	    gridBagConstraints4.insets = new Insets(10, 2, 2, 2);
 	    GridBagConstraints gridBagConstraints3 = new GridBagConstraints();
 	    gridBagConstraints3.gridx = 1;
-	    gridBagConstraints3.gridy = 2;
+	    gridBagConstraints3.gridy = 3;
 	    gridBagConstraints3.insets = new Insets(10, 2, 2, 2);
 	    GridBagConstraints gridBagConstraints2 = new GridBagConstraints();
 	    gridBagConstraints2.gridx = 0;
-	    gridBagConstraints2.gridy = 1;
+	    gridBagConstraints2.gridy = 2;
+	    gridBagConstraints2.anchor = GridBagConstraints.EAST;
 	    gridBagConstraints2.insets = new Insets(2, 2, 2, 2);
 	    jLabel = new JLabel();
 	    jLabel.setText("IP Address: ");
 	    GridBagConstraints gridBagConstraints1 = new GridBagConstraints();
 	    gridBagConstraints1.fill = GridBagConstraints.HORIZONTAL;
-	    gridBagConstraints1.gridy = 0;
+	    gridBagConstraints1.gridy = 1;
 	    gridBagConstraints1.gridwidth = 2;
 	    gridBagConstraints1.gridx = 1;
 	    gridBagConstraints1.insets = new Insets(2, 2, 2, 2);
 	    GridBagConstraints gridBagConstraints = new GridBagConstraints();
 	    gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
-	    gridBagConstraints.gridy = 1;
+	    gridBagConstraints.gridy = 2;
 	    gridBagConstraints.gridwidth = 2;
 	    gridBagConstraints.gridx = 1;
 	    gridBagConstraints.insets = new Insets(2, 2, 2, 2);
@@ -191,6 +215,8 @@ public class ConnectTo extends JDialog {
 	    jContentPane.add(connect(), gridBagConstraints3);
 	    jContentPane.add(cancel(), gridBagConstraints4);
 	    jContentPane.add(jLabel1, gridBagConstraints21);
+	    jContentPane.add(jLabel2, gridBagConstraints31);
+	    jContentPane.add(getConnectionList(), gridBagConstraints41);
 	}
 	return jContentPane;
     }
@@ -201,9 +227,30 @@ public class ConnectTo extends JDialog {
      * @return void
      */
     private void initialize() {
-	this.setSize(284, 138);
+	this.setSize(359, 146);
 	this.setContentPane(getJContentPane());
 	this.setTitle("Connect To Server");
+    }
+
+    /**
+     * This method initializes jComboBox
+     * 
+     * @return javax.swing.JComboBox
+     */
+    private JComboBox getConnectionList() {
+	if (connectionList == null) {
+	    connectionList = new JComboBox(new DefaultComboBoxModel(control
+		    .getConnections().toArray(new String[0])));
+	    connectionList.setSelectedIndex(0);
+	    connectionList.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent event) {
+
+		    ipAlias.setText("");
+		    addressField.setText("");
+		}
+	    });
+	}
+	return connectionList;
     }
 
 } // @jve:decl-index=0:visual-constraint="10,10"
