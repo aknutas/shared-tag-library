3b659b951f130c6ac70cc7da4f199756a797acc1
rmamunds@mtu.edu <rmamunds@mtu.edu@e62d969a-9d79-11de-8ef5-057bcddc142d>
2009-12-14 23:28:08 +0000

    javadoc and cosmetic changes Updated script generator

--- src/scripts/ScriptGenerator.java
+++ src/scripts/ScriptGenerator.java
@@ -1,4 +1,5 @@
 package scripts;
+
 import database.*;
 import data.*;
 import java.io.BufferedWriter;
@@ -10,173 +11,200 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.Vector;
 
-
 /**
  * Creates a Script generator for creating a random library
  * 
  * @author Robert Amundson
  */
 public class ScriptGenerator {
-	public Vector<String> dictionary;
-	private final File dictionaryFile;
-	private final File targetFile;
-	public Parser p;
-	Random numgen; 
-	/**
-	 * creates a script generator with a dictionary input
-	 * 
-	 * 
-	 * @param fileName the target dictionary file
-	 */
-	public ScriptGenerator(String fileName){
-		dictionary = new Vector<String>();
-		dictionaryFile = new File(fileName);  
-		targetFile = new File("src\\scripts\\books.txt");
-		numgen = new Random( 32564379 );
+    public Vector<String> seedDictionary;
+    private final File dictionaryFile;
+    private final File targetFile;
+    public Boolean importedDictionary;
+    Random numgen;
+
+    /**
+     * creates a ScriptGenerator for randomly generating a library save file
+     * 
+     * @param dictionaryLoc
+     *            the target dictionary file to be used as the random generation
+     * @param fileName
+     *            the ouput file
+     */
+    public ScriptGenerator(String dictionaryLoc, String fileName) {
+	seedDictionary = new Vector<String>();
+	dictionaryFile = new File(dictionaryLoc);
+	targetFile = new File(fileName);
+	numgen = new Random(32564379);
+	importedDictionary = false;
+    }
+
+    /**
+     * imports the seed dictionary for random generation
+     * 
+     * @throws FileNotFoundException
+     */
+    public void importSeedDictionary() throws FileNotFoundException {
+	processLineByLine();
+	if (!seedDictionary.isEmpty())
+	    importedDictionary = true;
+    }
+
+    /**
+     * Scanner Method for parsing into seedDictionary
+     * 
+     * @throws FileNotFoundException
+     */
+    protected final void processLineByLine() throws FileNotFoundException {
+	Scanner scanner = new Scanner(dictionaryFile);
+	try {
+	    while (scanner.hasNextLine()) {
+		processLine(scanner.nextLine());
+	    }
+	} catch (Exception e) {
+	    System.err.println(e);
+	} finally {
+	    scanner.close();
 	}
+    }
 
-	public static void main (String[] aguments){
-		ScriptGenerator sg = new ScriptGenerator("src\\scripts\\en_US.dic");
-		try{
-			sg.processLineByLine();
-		}
-		catch (Exception e){
-			System.err.println("error in s processline Dic "+ e);
-		}
-		sg.generateLibrary(10,5);
-		sg.p = new Parser("src\\scripts\\books.txt");
-
-		try{
-			sg.p.processLineByLine();
-		}
-		catch (Exception e){
-			System.err.println("error in s processline books "+ e);
-		}
-		
-		//at this point should contain a fully set up library with
-		// shelves containing 5 books each
-		/*Iterable<Bookshelf> booksf = p.lib.enumerateBookshelves();
-		Iterator<Bookshelf> iter = booksf.iterator();
-		Bookshelf bs;
-		while(iter.hasNext()){
-			bs = iter.next();
-			System.out.println("name: "+bs.getProperty("Name"));
-		}*/
-		
-		for(Bookshelf bs : sg.p.lib)
-			System.out.println("name: "+bs.getProperty("Name"));
-		
-		System.out.println("done");		
+    /**
+     * Scanner Method for parsing into seedDictionary
+     * 
+     * @throws FileNotFoundException
+     */
+    protected void processLine(String line) {
+	Scanner scanner = new Scanner(line);
+	scanner.useDelimiter("/");
+	if (scanner.hasNext()) {
+	    String aWord = scanner.next();
+	    seedDictionary.add(aWord);
+	} else {
+	    throw new IllegalArgumentException();
 	}
+	scanner.close();
+    }
 
-	public final void processLineByLine() throws FileNotFoundException {
-		Scanner scanner = new Scanner(dictionaryFile);
-		try {
-			while ( scanner.hasNextLine() ){
-				processLine( scanner.nextLine() );
+    /**
+     * Method for generation of a library save file at random
+     * 
+     * @param numbooks
+     *            the number of books in the text file
+     * @param shelfsize
+     *            number of books on a shelf
+     */
+    public void generateLibrary(int numbooks, int shelfsize) {
+	if (!importedDictionary)
+	    throw new IllegalArgumentException("Dictionary not Imported");
+	try {
+	    BufferedWriter output = new BufferedWriter(new FileWriter(
+		    targetFile));
+	    try {
+		for (int i = 0; i < numbooks; i++) {
+		    if (i % shelfsize == 0) {
+			if (i != 0) {
+			    output.write("ENDS:");
+			    output.newLine();
 			}
+			output.write(generateShelf());
+			output.newLine();
+		    }
+		    output.write(generateTitle());
+		    output.newLine();
+		    output.write(generateAuthor());
+		    output.newLine();
+		    output.write(generateTags());
+		    output.newLine();
+		    output.write("ENDB:");
+		    output.newLine();
 		}
-		catch (Exception e){
-			System.err.println(e);
-		}
-		finally {
-			scanner.close();
-		}
+		output.write("ENDS:");
+		output.newLine();
+	    } finally {
+		output.write("ENDLIB:");
+		output.newLine();
+		output.close();
+	    }
+	} catch (Exception e) {
+	    System.out.println("failed to generate library" + e);
 	}
+    }
 
-	protected void processLine(String line){
-		//use a second Scanner to parse the content of each line 
-		Scanner scanner = new Scanner(line);
-		scanner.useDelimiter("/");
-		if ( scanner.hasNext() ){
-			String aWord = scanner.next();
-			dictionary.add(aWord);
-		}
-		else {
-			throw new IllegalArgumentException();
-		}
-		//(no need for finally here, since String is source)
-		scanner.close();
-	}
-/**
- * 
- * @param numbooks the number of books in the text file
- * @param shelfsize number of books on a shelf
- */
-	public void generateLibrary(int numbooks,int shelfsize){
+    /**
+     * generates text for a random shelf
+     * 
+     * @return
+     */
+    private String generateShelf() {
+	String s = "SHELF:";
+	s = s.concat("Name");
+	s = s.concat(":"
+		+ seedDictionary.elementAt(numgen
+			.nextInt(seedDictionary.size())));
+	return s;
+    }
 
-		try{
-			BufferedWriter output = new BufferedWriter(new FileWriter(targetFile));
-			try {
-				//FileWriter always assumes default encoding is OK!
-				for(int i =0;i<numbooks;i++){
-					if(i%shelfsize==0){
-						if(i!=0){
-						output.write("ENDS:");
-						output.newLine();
-						}
-						output.write(generateShelf());						
-						output.newLine();
-					}
-					output.write(generateTitle());
-					output.newLine();
-					output.write(generateAuthor());
-					output.newLine();
-					output.write(generateGenre());
-					output.newLine();
-					output.write(generateTags());
-					output.newLine();
-					output.write("ENDB:");
-					output.newLine();
-				}
-				output.write("ENDS:");
-				output.newLine();
-			}
-			finally {
-				output.close();
-			}
-		}
-		catch(Exception e){
-			System.out.println("failed to generate library" +e);
-		}
-	}
-	private String generateShelf(){
-		String s="SHELF:";
-		s=s.concat("Name");
-		s=s.concat(" " + dictionary.elementAt(numgen.nextInt(dictionary.size())));
-		return s;
-	}
-	private String generateTitle(){
-		String s="TITLE:";
-		s=s.concat(dictionary.elementAt(numgen.nextInt(dictionary.size())));
-		s=s.concat(" " + dictionary.elementAt(numgen.nextInt(dictionary.size())));
-		s=s.concat(" " + dictionary.elementAt(numgen.nextInt(dictionary.size())));
-		return s;
-	}
-	private String generateAuthor(){
-		String s="AUTHOR:";
-		String[] honorific = {"Mr.","Mrs.","Ms.","Sir"};
-		s=s.concat(honorific[numgen.nextInt(4)]);
-		s=s.concat(" " + dictionary.elementAt(numgen.nextInt(dictionary.size())));
-		return s;
-	}
-	private String generateGenre(){
-		String s="GENRE:";
-		String[] genre = {"Horror","Scifi","Fantasy","Romance","Suspense","Mystery","Action","Historical","Non-Fiction","Blimp"};
-		s=s.concat(genre[numgen.nextInt(10)]);
-		return s;
-	}
-	private String generateTags(){
-		String s="TAG:";
-		s=s.concat(dictionary.elementAt(numgen.nextInt(dictionary.size())));
-		s=s.concat("," + dictionary.elementAt(numgen.nextInt(dictionary.size())));
-		s=s.concat("," + dictionary.elementAt(numgen.nextInt(dictionary.size())));
-		s=s.concat("," + dictionary.elementAt(numgen.nextInt(dictionary.size())));
-		s=s.concat("," + dictionary.elementAt(numgen.nextInt(dictionary.size())));
-		s=s.concat("," + dictionary.elementAt(numgen.nextInt(dictionary.size())));
-		s=s.concat("," + dictionary.elementAt(numgen.nextInt(dictionary.size())));
-		return s;
-	}
+    /**
+     * generates text for a random book title
+     * 
+     * @return
+     */
+    private String generateTitle() {
+	String s = "TITLE:";
+	s = s.concat(seedDictionary.elementAt(numgen.nextInt(seedDictionary
+		.size())));
+	s = s.concat(" "
+		+ seedDictionary.elementAt(numgen
+			.nextInt(seedDictionary.size())));
+	s = s.concat(" "
+		+ seedDictionary.elementAt(numgen
+			.nextInt(seedDictionary.size())));
+	return s;
+    }
 
+    /**
+     * generates text for a random author
+     * 
+     * @return
+     */
+    private String generateAuthor() {
+	String s = "AUTHOR:";
+	String[] honorific = { "Mr.", "Mrs.", "Ms.", "Sir" };
+	s = s.concat(honorific[numgen.nextInt(4)]);
+	s = s.concat(" "
+		+ seedDictionary.elementAt(numgen
+			.nextInt(seedDictionary.size())));
+	return s;
+    }
+
+    /**
+     * generates 6 random tags
+     * 
+     * @return
+     */
+    private String generateTags() {
+	String s = "TAG:";
+	s = s.concat(seedDictionary.elementAt(numgen.nextInt(seedDictionary
+		.size())));
+	s = s.concat(","
+		+ seedDictionary.elementAt(numgen
+			.nextInt(seedDictionary.size())));
+	s = s.concat(","
+		+ seedDictionary.elementAt(numgen
+			.nextInt(seedDictionary.size())));
+	s = s.concat(","
+		+ seedDictionary.elementAt(numgen
+			.nextInt(seedDictionary.size())));
+	s = s.concat(","
+		+ seedDictionary.elementAt(numgen
+			.nextInt(seedDictionary.size())));
+	s = s.concat(","
+		+ seedDictionary.elementAt(numgen
+			.nextInt(seedDictionary.size())));
+	s = s.concat(","
+		+ seedDictionary.elementAt(numgen
+			.nextInt(seedDictionary.size())));
+	return s;
+    }
 
 }
